143fb40b5c2d98627471b6bbe1f22cd5a050a5e9
rahatcnits <rahatcnits@gmail.com>
2023-08-31 16:28:39 +0600

    page setup

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,7 +7,7 @@ for (var i = 0; i < arrow.length; i++) {
 }
 
 let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
+let sidebarBtn = document.querySelector(".open_btn");
 console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
