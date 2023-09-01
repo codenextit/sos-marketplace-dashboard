26f253fa0974689e73fd5a9e2886c2cd545f7262
rahatcnits <rahatcnits@gmail.com>
2023-09-01 23:48:24 +0600

    dashboard work

---
+++ js/barChart.js
@@ -0,0 +1,146 @@
+// chart bar one
+var options = {
+  series: [
+    {
+      data: [
+        48, 22, 10, 28, 16, 21, 13, 30, 20, 28, 40, 18, 34, 28, 34, 45, 22, 32,
+        37, 25,
+      ],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "bar",
+    events: {
+      click: function (chart, w, e) {
+        // console.log(chart, w, e)
+      },
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#F3BE00", "#00BC8B", "#037BFF", "#FD4802"],
+  plotOptions: {
+    bar: {
+      columnWidth: "45%",
+      distributed: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  yaxis: {
+    labels: {
+      formatter: function (value) {
+        return "$" + value + "k";
+      },
+    },
+  },
+  xaxis: {
+    categories: [
+      "...Transport",
+      "...Security",
+      "...test and tag",
+      "...Temporary Fence",
+      "...Site Equipment",
+      "...Wet Hire",
+      "...Scaffold & Props",
+      "...Generator Hire",
+      "...Builders clean",
+      "...Design",
+      "...Connections",
+      "...Workshop & Tool Costs",
+      "...Traffic Management",
+      "...Safety",
+      "...First aid & spill kits",
+      "...civil works",
+      "...Inground services",
+      "...Structural Steel",
+      "...structure",
+      "...Brick & Blockwork",
+    ],
+    labels: {
+      style: {
+        fontSize: "12px",
+      },
+      rotate: -90,
+    },
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chartBarOne"), options);
+chart.render();
+
+// chart bar two
+var options = {
+  series: [
+    {
+      data: [
+        48, 22, 10, 28, 16, 21, 13, 30, 20, 28, 40, 18, 34, 28, 34, 45, 22, 32,
+        37, 25,
+      ],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "bar",
+    events: {
+      click: function (chart, w, e) {
+        // console.log(chart, w, e)
+      },
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#F3BE00", "#00BC8B", "#037BFF", "#FD4802"],
+  plotOptions: {
+    bar: {
+      columnWidth: "45%",
+      distributed: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: [
+      "Transport",
+      "Security",
+      "test and tag",
+      "Temporary Fence",
+      "Site Equipment",
+      "Wet Hire",
+      "Scaffold & Props",
+      "Generator Hire",
+      "Builders clean",
+      "Design",
+      "Connections",
+      "Workshop & Tool Costs",
+      "Traffic Management",
+      "Safety",
+      "First aid & spill kits",
+      "civil works",
+      "Inground services",
+      "Structural Steel",
+      "structure",
+      "Brick & Blockwork",
+    ],
+    labels: {
+      style: {
+        fontSize: "12px",
+      },
+      rotate: -90,
+    },
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chartBarTwo"), options);
+chart.render();
---
+++ js/pieChart.js
@@ -0,0 +1,78 @@
+// pie chart
+var options = {
+  series: [40, 20, 8, 8, 8, 8],
+  chart: {
+    width: 620,
+    type: "pie",
+  },
+  labels: [
+    "Youth 16-24",
+    "Mature aged",
+    "Ex justice",
+    "Asylum seeker/refugees",
+    "Long term unemployed",
+    "LGBTQI+",
+  ],
+  legend: {
+    show: true,
+    showForSingleSeries: false,
+    showForNullSeries: true,
+    showForZeroSeries: true,
+    position: "right",
+    // horizontalAlign: "left",
+    floating: false,
+    fontSize: "16px",
+    fontFamily: "Poppins",
+    fontWeight: 400,
+    formatter: undefined,
+    inverseOrder: false,
+    width: undefined,
+    height: undefined,
+    tooltipHoverFormatter: undefined,
+    customLegendItems: [],
+    offsetX: 0,
+    offsetY: 0,
+    labels: {
+      colors: ["#878787"],
+      useSeriesColors: false,
+    },
+    markers: {
+      width: 20,
+      height: 20,
+      strokeWidth: 0,
+      strokeColor: "#fff",
+      fillColors: undefined,
+      radius: 3,
+      customHTML: undefined,
+      onClick: undefined,
+      offsetX: 0,
+      offsetY: 0,
+    },
+    itemMargin: {
+      horizontal: 5,
+      vertical: 0,
+    },
+    onItemClick: {
+      toggleDataSeries: true,
+    },
+    onItemHover: {
+      highlightDataSeries: true,
+    },
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          position: "bottom",
+        },
+      },
+    },
+  ],
+};
+
+var chart = new ApexCharts(document.querySelector("#chartPie"), options);
+chart.render();
